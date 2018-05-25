3b6b2f8bc4ff2b74182acdc53d707b2acd671794
Kykung <59070071@it.kmitl.ac.th>
2018-05-25 18:16:12 +0700

    Arranged pre-training files

--- processXML.py
+++ processXML.py
@@ -1,19 +1,22 @@
 import xml.etree.ElementTree as ET
+import csv
+from os import listdir
+from os.path import isfile, join
 
-def read_xml(xml_file):
-
-    tree = ET.parse(xml_file)
+def read_xml(path):
+    tree = ET.parse(path)
     root = tree.getroot()
 
     return root
 
-def get_ojects(root):
 
-    cows = dict()
+def get_ojects(xml_file_path, images_dirpath):
+    root = read_xml(xml_file_path)
+    cows = list()
+
+    filename = root.find('filename').text
 
     for cow in root.findall('object'):
-        # Get cow ID
-        cow_id = cow.find('name').text
 
         # Get bounding box
         bbox = cow.find('bndbox')
@@ -23,6 +26,27 @@ def get_ojects(root):
         ymax = int(bbox.find('ymax').text)
 
         # Store into the cow collection
-        cows[cow_id] = {'xmin': xmin, 'xmax': xmax, 'ymin': ymin, 'ymax': ymax}
+        cows.append({'xmin': xmin, 'xmax': xmax, 'ymin': ymin, 'ymax': ymax, 'filename': join(images_dirpath, filename)})
 
     return cows
+
+
+def arrange_annotations_file(annotation_dirpath, images_dirpath):
+    files = [join(annotation_dirpath, i) for i in listdir(annotation_dirpath) if isfile(join(annotation_dirpath, i))]
+    cows = []
+
+    for file in files:
+        boundaries = get_ojects(file, images_dirpath)
+        for boundary in boundaries:
+            cows.append(boundary)
+
+    with open('annotations.csv', 'w') as csvfile:
+        filewriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        for cow in cows:
+            print(cow)
+            for i in cow:
+                cow[i] = str(cow[i])
+            filewriter.writerow([cow['filename'], cow['xmax'], cow['ymax'], cow['xmin'], cow['ymin'], 'cow'])
+
+
+arrange_annotations_file('./Dataset/FriesianCattle2017/annotations', './Dataset/FriesianCattle2017/images')
