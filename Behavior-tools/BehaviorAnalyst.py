1dec1ad623b3f9fe18254d1dd3a42650baf2632c
Kykung <59070071@it.kmitl.ac.th>
2018-12-25 23:51:38 +0700

    :construction: Implement behavior analysis class to be workable

--- Behavior-tools/BehaviorAnalyst.py
+++ Behavior-tools/BehaviorAnalyst.py
@@ -5,23 +5,34 @@
 # Dependencies for behavioral analysis
 from sklearn.metrics.pairwise import euclidean_distances
 
+# Distance cutoff for heat possibility
+DISTANCE_CUTOFF = 100
+
 class BehaviorAnalyst:
-	def __init__(self):
 
-	def calculateDistance(self, point_a, point_b):
-		return euclidean_distances(point_a, point_b)[0][0]
+    def calculateDistance(self, point_a, point_b):
+        return euclidean_distances(point_a, point_b)[0][0]
 
-	def verifyBodyPart(self, part_a, part_b):
-		pass
+    def verifyBodyPart(self, part_a, part_b):
+        pass
 
-	def examineDistance(self, dist):
-		pass
+    def examineDistance(self, dist):
+        if dist >= DISTANCE_CUTOFF:
+            return False
+        return True
 
-	def analyzeHeatProbOnFrame(self, examined_result):
+    def analyzeHeatProbOnFrame(self, point_a, point_b):
 
-		if examined_result:
-			# heat state is True, and CS is increased
-		else:
-			# heat state is False, and CS is decreased
+        dist = self.calculateDistance(point_a, point_b)
+        examined_result = self.examineDistance(dist)
 
-		# Save state
+        # print("----> Dist =", dist)
+
+        if examined_result:
+            # heat state is True, and CS is increased
+            # print("----> ++")
+            return True, 1
+        else:
+            # heat state is False, and CS is decreased
+            # print("----> --")
+            return False, -1
